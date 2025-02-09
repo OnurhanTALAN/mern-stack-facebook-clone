0a76d1330c62d3795e2c067aec1fa168cfc55d13
OnurhanTalan <o.talan2021@gtu.edu.tr>
2025-02-10 01:21:45 +0300

    base file structure and dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="w-full h-full bg-blue-400 text-center font-extrabold text-3xl text-white">Hello world</p>
+    </>
+  )
+}
+
+export default App
